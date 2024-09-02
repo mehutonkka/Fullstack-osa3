29521cc74d4c51d0d1bcc0cf3afc2aad4b810180
mehutonkka <mishka.hagelberg@gmail.com>
2024-09-02 18:59:48 +0300

    uibuild

--- puhelinluettelo_backend/models/person.js
+++ puhelinluettelo_backend/models/person.js
@@ -24,6 +24,12 @@ const personSchema = new mongoose.Schema({
     number: {
         type: String,
         minlength: 8,
+        validate: {
+            validator: function(v) {
+                return /^\d{2,3}-\d+$/.test(v);
+            },
+            message: props => `${props.value} has to contain only numbers and consist of 2 parts separated with '-' with the first part being 2-3 digits long`
+        },
         required: true
     }
 })
