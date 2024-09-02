8ba3aaa066dd6ac4f75d08f0c96e05e882bc49cc
mehutonkka <mishka.hagelberg@gmail.com>
2024-09-02 16:02:47 +0300

    uibuild

--- puhelinluettelo_backend/index.js
+++ puhelinluettelo_backend/index.js
@@ -9,8 +9,10 @@ const Person = require('./models/person')
 
 const app = express()
 
-app.use(express.json())
 app.use(express.static('dist'))
+app.use(express.json())
+
+
 
 app.use(cors())
 
@@ -20,6 +22,8 @@ morgan.token('person', function getPerson (request) {
 })
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :person'))
+
+
 
 
 const infoText = (date) => {
@@ -51,7 +55,7 @@ app.get('/api/persons', (request, response) => {
   
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
     Person.findById(request.params.id).then(person => {
       if (person) {
         response.json(person)
@@ -59,6 +63,7 @@ app.get('/api/persons/:id', (request, response) => {
         response.status(404).end()
       }
     })
+    .catch(error => next(error))
     
   })
 
@@ -104,12 +109,35 @@ app.get('/api/persons/:id', (request, response) => {
 
 
 app.delete('/api/persons/:id', (request, response) => {
-  Person.find({}).then(persons => {
-    const id = request.params.id
-    persons = persons.filter(person => person.id !== id)
+  Person.findByIdAndDelete(request.params.id)
+    .then(result => {
     response.status(204).end()
     })
+    .catch(error => next(error))
   })
+
+
+
+  const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+  }
+  
+  
+  app.use(unknownEndpoint)
+
+  const errorHandler = (error, request, response, next) => {
+    console.log(error.message)
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    }
+  
+    next(error)
+  }
+  
+  app.use(errorHandler)
+
+
+
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
